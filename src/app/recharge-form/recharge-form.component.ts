03a4a3ea3e2c4314babc764f0f1c691cf360cd7e
Xi Wu <XWu35@slb.com>
2024-11-05 19:07:41 +0800

    feat: move phone-input to phone-input

--- src/app/recharge-form/recharge-form.component.ts
+++ src/app/recharge-form/recharge-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PhoneInputComponent } from "../phone-input/phone-input.component";
 
 @Component({
   selector: 'app-recharge-form',
   standalone: true,
-  imports: [],
+  imports: [PhoneInputComponent],
   templateUrl: './recharge-form.component.html',
   styleUrl: './recharge-form.component.css'
 })
