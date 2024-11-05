9e36cfad17403a056bfbfed3e2b5bf8e64803f3d
yyu26 <yyu26@slb.com>
2024-11-05 19:33:47 +0800

    feat: move steps to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {SubmitButtonComponent} from "./components/submit-button/submit-button.component";
 import {RechargeDetailsComponent} from "./components/recharge-details/recharge-details.component";
 import {PhoneNumInputComponent} from "./components/phone-num-input/phone-num-input.component";
+import {RechargeStepsComponent} from "./components/recharge-steps/recharge-steps.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SubmitButtonComponent, RechargeDetailsComponent, PhoneNumInputComponent],
+  imports: [RouterOutlet, SubmitButtonComponent, RechargeDetailsComponent, PhoneNumInputComponent, RechargeStepsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
