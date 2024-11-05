c13abb405c062b87b9d83be84c594de5f483a3fb
Xi Wu <XWu35@slb.com>
2024-11-05 19:13:02 +0800

    feat: move discount to  money-calculate

--- src/app/recharge-form/recharge-form.component.ts
+++ src/app/recharge-form/recharge-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PhoneInputComponent } from "../phone-input/phone-input.component";
+import { MoneyCalculateComponent } from "../money-calculate/money-calculate.component";
 
 @Component({
   selector: 'app-recharge-form',
   standalone: true,
-  imports: [PhoneInputComponent],
+  imports: [PhoneInputComponent, MoneyCalculateComponent],
   templateUrl: './recharge-form.component.html',
   styleUrl: './recharge-form.component.css'
 })
