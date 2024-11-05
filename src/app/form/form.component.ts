4e224f71994be197a649ef83cb4fbd17b47dfdf1
zjiang10 <zjiang10@slb.com>
2024-11-05 19:23:29 +0800

    feat: add dynamic discount-tag

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './form.component.html',
   styleUrl: '../app.component.css'
 })
-export class FormComponent {
+export class FormComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const discountSelect = document.getElementById('discount') as HTMLSelectElement;
+    const discountTags = document.querySelectorAll('.discount-tag');
 
+    discountSelect.addEventListener('change', function() {
+      const discountValue = discountSelect.value;
+      console.log(discountValue)
+      discountTags.forEach(tag => {
+        (tag as HTMLElement).style.display = 'none';
+      });
+      const selectedTag = document.getElementById('tag-' + discountValue) as HTMLElement;
+      console.log(selectedTag)
+      if (selectedTag) {
+        selectedTag.style.display = 'inline-block'; // 显示选中的折扣标签
+      }
+    });
+
+    // 页面加载时触发 change 事件以设置初始状态
+    discountSelect.dispatchEvent(new Event('change'));
+  }
 }
