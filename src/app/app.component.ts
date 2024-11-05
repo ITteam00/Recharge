e5201711b4d8bc36d56c9fbc7a04325b38265088
yyu26 <yyu26@slb.com>
2024-11-05 19:28:49 +0800

    feat: move phone-num-input to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SubmitButtonComponent} from "./components/submit-button/submit-button.component";
 import {RechargeDetailsComponent} from "./components/recharge-details/recharge-details.component";
+import {PhoneNumInputComponent} from "./components/phone-num-input/phone-num-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SubmitButtonComponent, RechargeDetailsComponent],
+  imports: [RouterOutlet, SubmitButtonComponent, RechargeDetailsComponent, PhoneNumInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
