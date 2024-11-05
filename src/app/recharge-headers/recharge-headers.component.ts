8ff5dc21121db6abdf167dc181f78b523d8149b4
Xi Wu <XWu35@slb.com>
2024-11-05 18:46:23 +0800

    feat: add rechargeheaders component

---
+++ src/app/recharge-headers/recharge-headers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recharge-headers',
+  standalone: true,
+  imports: [],
+  templateUrl: './recharge-headers.component.html',
+  styleUrl: './recharge-headers.component.css'
+})
+export class RechargeHeadersComponent {
+
+}
