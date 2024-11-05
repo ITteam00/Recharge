b9acea4df5575f8225d5e03a6f52155adf0b4eb0
Xi Wu <XWu35@slb.com>
2024-11-05 19:14:57 +0800

    feat: move submit-btn to submit-btn

--- src/app/recharge-form/recharge-form.component.ts
+++ src/app/recharge-form/recharge-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PhoneInputComponent } from "../phone-input/phone-input.component";
 import { MoneyCalculateComponent } from "../money-calculate/money-calculate.component";
+import { RechargeButtonComponent } from "../recharge-button/recharge-button.component";
 
 @Component({
   selector: 'app-recharge-form',
   standalone: true,
-  imports: [PhoneInputComponent, MoneyCalculateComponent],
+  imports: [PhoneInputComponent, MoneyCalculateComponent, RechargeButtonComponent],
   templateUrl: './recharge-form.component.html',
   styleUrl: './recharge-form.component.css'
 })
