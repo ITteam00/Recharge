baccababf25282959ef11b2ee415c997ab5495d7
kgaohahaha <15296612347@163.com>
2024-11-05 21:09:03 +0800

    feat: money caculater

--- src/app/money-caculater/money-caculater.component.ts
+++ src/app/money-caculater/money-caculater.component.ts
@@ -15,4 +15,55 @@ export class MoneyCaculaterComponent {
     "Discount - Mobile Store Recharge 8.0% Discount"
   ];
   selectedType = this.promotionTypes[0];
+  discount: number = 0
+  paymentAmount: number | null = null
+  amountReceived: number | null = null
+  inputValue: number | null = null
+
+
+
+  public updateDiscount() {
+    if (this.selectedType === "Discount - Mobile Store Recharge 9.0% Discount") {
+      this.discount = 0.09
+    }
+    if (this.selectedType === "Discount - Mobile Store Recharge 8.0% Discount") {
+      this.discount = 0.08
+    }
+    if (this.selectedType === "None") {
+      this.discount = 0
+    }
+  }
+
+
+  onDiscountChange() {
+    this.updateDiscount();
+    this.calculateAmount();
+  }
+
+  setAmount(amount: number) {
+    this.amountReceived = amount;
+    this.calculateAmount()
+  }
+
+  calculateAmount() {
+    this.paymentAmount = (this.amountReceived as number) * (1 - this.discount);
+  }
+
+
+  checkValue() {
+    if (this.inputValue === null || this.inputValue === undefined) {
+      this.inputValue = null;
+    }
+    else {
+      this.amountReceived = this.inputValue
+      this.calculateAmount()
+    }
+  }
+
+  clearValue() {
+    if (this.inputValue === null) {
+      this.inputValue = null;
+    }
+  }
+
 }
