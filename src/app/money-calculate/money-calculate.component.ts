b7de69ab15b140aafe5ee4551fb00b2bd1d8ff4f
Xi Wu <XWu35@slb.com>
2024-11-05 19:42:08 +0800

    feat: add Discount selection

--- src/app/money-calculate/money-calculate.component.ts
+++ src/app/money-calculate/money-calculate.component.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './money-calculate.component.css'
 })
 export class MoneyCalculateComponent {
+  selectedAmount: number = 100;
+  discountRate: number = 0;
+  paymentAmount: number = 100;
+  receivedAmount: number = 100;
 
+  updateDiscount(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.discountRate = parseFloat(selectElement.value);
+    this.updateAmounts();
+  }
+
+  selectAmount(amount: number) {
+    this.selectedAmount = amount;
+    this.updateAmounts();
+  }
+
+  updateCustomAmount(value: string) {
+    const amount = parseFloat(value);
+    if (!isNaN(amount) && amount >= 10 && amount <= 3000) {
+      this.selectedAmount = amount;
+      this.updateAmounts();
+    }
+  }
+
+  updateAmounts() {
+    this.paymentAmount = this.selectedAmount * (1 - this.discountRate / 100);
+    this.receivedAmount = this.selectedAmount;
+  }
 }
