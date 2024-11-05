078895430cb285ba610cc802062a9af07c98bd38
zjiang10 <zjiang10@slb.com>
2024-11-05 18:45:13 +0800

    feat:init comps

---
+++ src/app/start-recharge-button/start-recharge-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-recharge-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-recharge-button.component.html',
+  styleUrl: './start-recharge-button.component.css'
+})
+export class StartRechargeButtonComponent {
+
+}
