5035a0087af834a3ce15503b2b4d784716b224fd
yyu26 <yyu26@slb.com>
2024-11-05 21:18:27 +0800

    feat: init new page component

---
+++ src/app/components/all-recharge-info/all-recharge-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-recharge-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-recharge-info.component.html',
+  styleUrl: './all-recharge-info.component.css'
+})
+export class AllRechargeInfoComponent {
+
+}
---
+++ src/app/components/recharge-confirmation/recharge-confirmation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recharge-confirmation',
+  standalone: true,
+  imports: [],
+  templateUrl: './recharge-confirmation.component.html',
+  styleUrl: './recharge-confirmation.component.css'
+})
+export class RechargeConfirmationComponent {
+
+}
