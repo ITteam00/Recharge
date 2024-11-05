046388198ed168076351dce0cdf2bce7710313e5
yyu26 <yyu26@slb.com>
2024-11-05 19:55:28 +0800

    feat: add phone number input logic in ts

--- src/app/components/phone-num-input/phone-num-input.component.ts
+++ src/app/components/phone-num-input/phone-num-input.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-phone-num-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgClass
+  ],
   templateUrl: './phone-num-input.component.html',
   styleUrl: './phone-num-input.component.css'
 })
 export class PhoneNumInputComponent {
+  @Input() phoneNumber: string = '';
+  @Input() location: string = 'Beijing';
+  @Output() phoneNumberChange = new EventEmitter<string>();
 
+  isValidPhoneNumber: boolean = true;
+
+  validatePhoneNumber() {
+    const phonePattern = /^1\d{10}$/; //
+    this.isValidPhoneNumber = phonePattern.test(this.phoneNumber);
+    if (this.isValidPhoneNumber) {
+      this.phoneNumberChange.emit(this.phoneNumber);
+    } else {
+      this.phoneNumberChange.emit('');
+    }
+  }
 }
