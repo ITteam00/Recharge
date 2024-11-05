a8994862f7bc777a95d3c401de11cddce0dbc28c
Xi Wu <XWu35@slb.com>
2024-11-05 18:45:48 +0800

    feat: add recharge component

---
+++ src/app/recharge-form/recharge-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recharge-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './recharge-form.component.html',
+  styleUrl: './recharge-form.component.css'
+})
+export class RechargeFormComponent {
+
+}
