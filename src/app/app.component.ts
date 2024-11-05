4900214977afe72a1bb6857aad6dbb6da36c9e4d
yyu26 <yyu26@slb.com>
2024-11-05 19:21:21 +0800

    feat: move submit button to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SubmitButtonComponent} from "./components/submit-button/submit-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SubmitButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
