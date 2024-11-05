97905b8a050760c5e36567aff1782b530e1e921d
yyu26 <yyu26@slb.com>
2024-11-05 19:25:16 +0800

    feat: move recharge-details to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SubmitButtonComponent} from "./components/submit-button/submit-button.component";
+import {RechargeDetailsComponent} from "./components/recharge-details/recharge-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SubmitButtonComponent],
+  imports: [RouterOutlet, SubmitButtonComponent, RechargeDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
