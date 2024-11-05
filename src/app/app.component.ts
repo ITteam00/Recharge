1d30f55618fea38485d0f1ebe6eaaf3e87a9a529
Xi Wu <XWu35@slb.com>
2024-11-05 19:05:09 +0800

    feat: move recharge-form to recharge-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RechargeHeadersComponent } from "./recharge-headers/recharge-headers.component";
+import { RechargeFormComponent } from "./recharge-form/recharge-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RechargeHeadersComponent],
+  imports: [RouterOutlet, RechargeHeadersComponent, RechargeFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
